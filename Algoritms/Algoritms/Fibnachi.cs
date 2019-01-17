30cd6e1b083b307e9d1aff06c741a48eda22c21e
ram-smart <ramsmart@mail.ru>
2019-01-18 00:46:09 +0500

    added new algoritms

---
+++ Algoritms/Algoritms/Fibnachi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritms
+{
+    public class Fibnachi
+    {
+        public int FibonachiRecursion(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+
+            return FibonachiRecursion(n - 1) + FibonachiRecursion(n - 2);
+        }
+
+        public void FibbonachiArray(int n)
+        {
+            var arrayNumbers = new int[n];
+            arrayNumbers[0] = 0;
+            arrayNumbers[1] = 1;
+            arrayNumbers[2] = 1;
+
+            var j1 = 1;
+            var j2 = 2;
+
+            for (var i = 3; i <= arrayNumbers.Length - 1; i++, j1++, j2++)
+            {
+                arrayNumbers[i] = arrayNumbers[j1] + arrayNumbers[j2];
+            }
+
+            foreach (var m in arrayNumbers)
+            {
+                Console.WriteLine(m);
+            }
+        }
+
+    }
+}
---
+++ Algoritms/Algoritms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var fib = new Fibnachi();
+            //fib.FibbonachiArray(8);
+
+            var scip = new ScipHexlet();
+            Console.WriteLine(scip.F(1));
+        }
+    }
+}
